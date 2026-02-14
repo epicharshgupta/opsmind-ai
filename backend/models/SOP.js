a6ab720c2299ee2628d0fe7451fffb1e93dd6f84
epicharshgupta <harshitguptapranjul@gmail.com>
2026-02-14 17:49:05 +0530

    Create SOP schema model

---
+++ backend/models/SOP.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const sopSchema = new mongoose.Schema({
+  filename: String,
+  filepath: String,
+  text: String,
+  uploadedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("SOP", sopSchema);
