fc53deb961aa21d25b52c8e80e355a2363203ab4
epicharshgupta <harshitguptapranjul@gmail.com>
2026-02-14 17:49:53 +0530

    Add PDF text extraction utility

---
+++ backend/utils/extractText.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const pdfParse = require("pdf-parse");
+
+async function extractText(filePath) {
+  const dataBuffer = fs.readFileSync(filePath);
+  const data = await pdfParse(dataBuffer);
+  return data.text;
+}
+
+module.exports = extractText;
