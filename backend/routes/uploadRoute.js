f0d3250d7cbb8317a37f6cb539b090a19ea33170
epicharshgupta <harshitguptapranjul@gmail.com>
2026-02-15 18:12:07 +0530

    adding embeddings routes and data

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -4,19 +4,26 @@ const upload = require("../multerConfig");
 const SOP = require("../models/SOP");
 const extractText = require("../utils/extractText");
 const chunkText = require("../utils/chunkText");
+const generateEmbedding = require("../utils/generateEmbedding");
 
 router.post("/upload", upload.single("file"), async (req, res) => {
   try {
     // PDF se text extract karo
     const extractedText = await extractText(req.file.path);
     const chunks = chunkText(extractedText);
+    const embeddings = [];
 
+    for (let chunk of chunks) {
+      const embedding = await generateEmbedding(chunk);
+      embeddings.push(embedding);
+    }
     // DB me save karo
     const newFile = new SOP({
       filename: req.file.filename,
       filepath: req.file.path,
       text: extractedText,
-      chunks: chunks
+      chunks: chunks,
+      embeddings: embeddings
     });
 
     await newFile.save();
